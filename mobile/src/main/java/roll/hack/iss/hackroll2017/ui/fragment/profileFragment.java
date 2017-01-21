769116f17468481e6cdfeb8583123fe3d0e239c3
LinBoYuan <boyuan_lin@yahoo.com>
2017-01-21 20:30:13 +0800

    some confiure file uplaod

--- mobile/src/main/java/roll/hack/iss/hackroll2017/ui/fragment/profileFragment.java
+++ mobile/src/main/java/roll/hack/iss/hackroll2017/ui/fragment/profileFragment.java
@@ -1,8 +1,25 @@
 package roll.hack.iss.hackroll2017.ui.fragment;
 
+import roll.hack.iss.hackroll2017.R;
+import roll.hack.iss.hackroll2017.ui.base.BaseFragment;
+
 /**
  * Created by linby on 21/01/2017.
  */
 
-public class profileFragment {
+public class ProfileFragment extends BaseFragment{
+    @Override
+    protected int getLayoutId() {
+        return R .layout.frag_profile;
+    }
+
+    @Override
+    protected void initComponent() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
