d772e287f4e6030a039f74c8e986de7756126aeb
Samrat <samrat2702@gmail.com>
2017-01-22 01:54:38 +0800

    Added fourth item details.

--- mobile/src/main/java/roll/hack/iss/hackroll2017/App.java
+++ mobile/src/main/java/roll/hack/iss/hackroll2017/App.java
@@ -141,23 +141,18 @@ public class App extends Application implements TextToSpeech.OnInitListener {
 
 
         Recipe recipe4 = new Recipe();
-        recipe4.setId(110920);
-        recipe4.setName("Spanish Tortilla with Broccoli, Chorizo, and Onion");
-        recipe4.setImgPath("https://spoonacular.com/recipeImages/spanish-tortilla-with-broccoli-chorizo-and-onion-2-110920.png");
-        recipe4.setRating(2);
-        recipe4.setTimeToCook(30);
-        recipe4.setCalorieCount(481);
-        recipe4.setComplexity("Difficult");
-        recipe4.setDetailedSteps("Step 1: In a large bowl, whisk eggs together with 1 teaspoon salt and 1/2 teaspoon black pepper. Set aside." +
-                "Step 2: Heat half of olive oil in a 10-inch non-stick or cast iron skillet over high heat until simmering." +
-                "Step 3: Add broccoli and cook without moving until well charred on one side, about 2 minutes. Reduce heat to medium and continue to cook, stirring occasionally, until broccoli is very tender, about 5 minutes longer." +
-                "Step 4: Add onions and chorizo, season to taste with salt and pepper, reduce heat to medium, and continue to cook, stirring frequently, until onions and broccoli are completely softened, about 5 minutes longer." +
-                "Step 5: Add broccoli/onion mixture to bowl with eggs and stir immediately to combine. Return skillet to heat and add remaining 1/4 cup olive oil." +
-                "Step 6: Heat until shimmering." +
-                "Step 7: Add egg mixture and cook, stirring gently with a wooden spoon or rubber spatula until eggs are about 1/3 set, about 1 minute. Continue to cook, swirling pan gently until eggs are set about 2/3 of the way through and base of tortilla is deep golden brown, about 5 minute longer." +
-                "Step 8: Place a large plate over the skillet and using both hands and two kitchen towels, invert skillet and plate so that tortilla flips out onto the plate with the cooked-side up. Carefully slide back into skillet. Continue to cook, gently swirling pan until tortilla is golden brown on second side, about 4 minutes longer. For a perfect shape, invert onto plate and slide back into skillet 4 to 5 more times." +
-                "Step 9: Transfer to a cutting board and let rest at least 5 minutes and up to 3 hours before slicing and serving with allioli."
-
+        recipe4.setId(172369);
+        recipe4.setName("Chicken Salad-Stuffed Tomato Appetizers");
+        recipe4.setImgPath("https://spoonacular.com/recipeImages/chicken-salad-stuffed-tomato-appetizers-172369.jpg");
+        recipe4.setRating(5);
+        recipe4.setTimeToCook(35);
+        recipe4.setCalorieCount(60);
+        recipe4.setComplexity("Easy");
+        recipe4.setDetailedSteps("Step 1: With sharp knife, cut 1/4-inch off stem side of each tomato and 1/8-inch off bottoms. Using melon baller or small spoon, remove insides of tomatoes; discard." +
+                "Step 2: Make cuts in ends of each green onion piece; place in bowl of ice water until curly." +
+                "Step 3: In small bowl, mix chicken salad and horseradish." +
+                "Step 4: Place 1 teaspoon chicken salad in each tomato." +
+                "Step 5: Place 1 green onion brush in top center of each tomato for stem."
         );
 
     }
